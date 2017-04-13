5e5aab932b37307a6ffc1ddc3cea89fb80da344f
willclarktech <will@willclark.tech>
2017-04-13 20:40:09 +0200

    add concealAndReveal example

---
+++ examples/concealAndReveal.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const {
+  conceal,
+  reveal,
+} = require('./index')
+
+////////////////////////////////////////////////////////////////
+// CONCEAL
+////////////////////////////////////////////////////////////////
+
+const [,, messageFile, inputFile, outputFile] = process.argv
+const message = messageFile
+  ? fs.readFileSync(messageFile)
+  : 'just testing'
+const original = fs.readFileSync(inputFile || './tmp/input.png')
+const concealed = conceal(original, message)
+fs.writeFileSync(outputFile || './tmp/output.png', concealed)
+
+////////////////////////////////////////////////////////////////
+// REVEAL
+////////////////////////////////////////////////////////////////
+
+const image = fs.readFileSync('./tmp/output.png')
+const revealed = reveal(image, 'utf8')
+console.info('message was:', revealed)
