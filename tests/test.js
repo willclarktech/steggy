2e51c3be4eba9e7668d0d7c698ce840ce2574a63
willclarktech <will@willclark.tech>
2017-04-15 01:09:06 +0200

    rewrite to embed message length in rgb not alpha

--- tests/test.js
+++ tests/test.js
@@ -2,35 +2,11 @@
 const fs = require('fs')
 const assert = require('assert')
 const {
-  getMessageIndex,
   conceal,
 } = require('../lib/conceal')
 const {
   reveal,
 } = require('../lib/reveal')
-
-const testGetMessageIndex = () => {
-  const testCorrectOutputs = () =>
-    [
-      [0, 0],
-      [1, 1],
-      [2, 2],
-      [4, 3],
-      [5, 4],
-      [12, 9],
-      [13, 10],
-    ].forEach(([input, output]) =>
-      assert.strictEqual(getMessageIndex(input), output)
-    )
-
-  const testInvalidInputs = () =>
-    [3, 7, 11, 103].forEach(input =>
-      assert.throws(getMessageIndex.bind(null, input), /Cannot get bit index/)
-    )
-
-  testCorrectOutputs()
-  testInvalidInputs()
-}
 
 const testEndToEnd = () => {
   const testWithBuffers = () => {
@@ -38,7 +14,9 @@ const testEndToEnd = () => {
     const message = Buffer.from('testing testing 123')
 
     const concealed = conceal(image, message)
+    fs.writeFileSync('./tmp/TEST.png', concealed)
     const result = reveal(concealed)
+    console.log('message was:', result.toString())
 
     return assert.ok(result.equals(message))
   }
@@ -48,5 +26,4 @@ const testEndToEnd = () => {
 
 [
   testEndToEnd,
-  testGetMessageIndex,
 ].forEach(test => test())
