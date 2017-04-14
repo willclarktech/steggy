4fecb10b2bff9505663c2cf58c1568d404a19039
willclarktech <will@willclark.tech>
2017-04-14 02:10:39 +0200

    restructure test file

--- tests/test.js
+++ tests/test.js
@@ -2,30 +2,35 @@
 const assert = require('assert')
 const {
   getBitIndex,
-  // conceal,
 } = require('../lib/conceal')
-const {
-  // reveal,
-} = require('../lib/reveal')
 
 const testGetBitIndex = () => {
-  const correct = [
-    [0, 0],
-    [1, 1],
-    [2, 2],
-    [4, 3],
-    [5, 4],
-    [12, 9],
-    [13, 10],
-  ]
-  const invalid = [3, 7, 11, 103]
+  const testCorrectOutputs = () => {
+    const correct = [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+      [4, 3],
+      [5, 4],
+      [12, 9],
+      [13, 10],
+    ]
 
-  correct.forEach(([input, output]) =>
-    assert.strictEqual(getBitIndex(input), output)
-  )
-  invalid.forEach(input =>
-    assert.throws(getBitIndex.bind(null, input), /Cannot get bit index/)
-  )
+    correct.forEach(([input, output]) =>
+      assert.strictEqual(getBitIndex(input), output)
+    )
+  }
+
+  const testInvalidInputs = () => {
+    const invalid = [3, 7, 11, 103]
+
+    invalid.forEach(input =>
+      assert.throws(getBitIndex.bind(null, input), /Cannot get bit index/)
+    )
+  }
+
+  testCorrectOutputs()
+  testInvalidInputs()
 }
 
 [
